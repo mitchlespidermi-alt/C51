5f67f75a3dc1ccddf4594de9b6bf607e948f6eb3
mitchlespidermi-alt <mitchle.spidermi@gmail.com>
2026-04-09 10:43:36 +0800

    Add files via upload

---
+++ tim.c
@@ -0,0 +1,42 @@
+#include "tim.h"
+
+//void Timer0Init(void)		//1毫秒@12.000MHz
+//{
+//	AUXR |= 0x80;		//定时器时钟1T模式
+//	TMOD &= 0xF0;		//设置定时器模式
+//	TL0 = 0x20;		//设置定时初值
+//	TH0 = 0xD1;		//设置定时初值
+//	TF0 = 0;		//清除TF0标志
+//	TR0 = 1;		//定时器0开始计时
+//	ET0 = 1;
+//}
+
+void Timer1Init(void)		//1毫秒@12.000MHz
+{
+	AUXR |= 0x40;		//定时器时钟1T模式
+	TMOD &= 0x0F;		//设置定时器模式
+	TL1 = 0x20;		//设置定时初值
+	TH1 = 0xD1;		//设置定时初值
+	TF1 = 0;		//清除TF1标志
+	TR1 = 1;		//定时器1开始计时
+    ET1 = 1;
+}
+
+//void Timer0Init(void)
+//{
+//    TMOD &= 0xF0;
+//    TMOD |= 0x05;
+//    TL0 = 0;
+//    TH0 = 0;
+//    TR0 = 1;
+//}
+
+void Timer0Init(void)		//12微秒@12.000MHz
+{
+	AUXR &= 0x7F;		//定时器时钟12T模式
+	TMOD &= 0xF0;		//设置定时器模式
+	TL0 = 0xF4;		//设置定时初值
+	TH0 = 0xFF;		//设置定时初值
+	TF0 = 0;		//清除TF0标志
+	TR0 = 0;		//定时器0不开始计时
+}
